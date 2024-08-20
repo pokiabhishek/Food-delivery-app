e31fd7c0b2f80b9f5b101230991b2f4fe577a965
Your Name <your.email@example.com>
2024-08-21 03:02:46 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base:"/Food-delivery-app/",
   server: {
     host: '0.0.0.0', // This allows access from external devices
     port: 3000,      // or any port you are using
